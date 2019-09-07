d77ab4cd10bc1e51ad7b04d4719eec87b53ad987
Genkzsz11 <genkzsz11@gmail.com>
2019-09-08 04:53:40 +0700

    Remove Screenrecord tile

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -49,7 +49,6 @@ import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
-import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -115,8 +114,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new NfcTile(mHost);
             case "screenshot":
                 return new ScreenshotTile(mHost);
-            case "screenrecord":
-                return new ScreenrecordTile(mHost);
             case "immersive":
                 return new ImmersiveTile(mHost);
             case "font":
