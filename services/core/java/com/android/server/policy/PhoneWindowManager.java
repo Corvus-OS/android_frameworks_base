BlissRoms/platform_frameworks_base

ScreenshotTile: add option for long screenshot
Change-Id: I2a157747e3254b197e5cb2b5de14e40db28eebda

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6359,6 +6359,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mContext.enforceCallingOrSelfPermission(Manifest.permission.ACCESS_SURFACE_FLINGER,
                         TAG + "sendCustomAction permission denied");
                 takeScreenshot(TAKE_SCREENSHOT_SELECTED_REGION);
+            } else if (BlissUtils.INTENT_LONG_SCREENSHOT.equals(action)) {
+                mContext.enforceCallingOrSelfPermission(Manifest.permission.ACCESS_SURFACE_FLINGER,
+                        TAG + "sendCustomAction permission denied");
+                takeAlternativeScreenshot();
             }
         }
     }

Corvus-OS/android_frameworks_base

Signed-off-by: pengus77 <ivan@cerrato.biz>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6380,6 +6380,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mHandler.removeCallbacks(mScreenshotRunnable);
                 mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_SELECTED_REGION);
                 mHandler.post(mScreenshotRunnable);
+            } else if (ActionUtils.INTENT_LONG_SCREENSHOT.equals(action)) {
+                mContext.enforceCallingOrSelfPermission(Manifest.permission.ACCESS_SURFACE_FLINGER,
+                        TAG + "sendCustomAction permission denied");
+                takeAlternativeScreenshot();
             }
         }
     }