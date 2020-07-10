62d7c28c7871c3cdb55d0515c25d2f0c1d08e756
Pranav Vashi <neobuddy89@gmail.com>
2020-07-10 20:48:33 +0530

    Clock: Ensure mHandler is not null
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>
    Change-Id: I07eb1c660e04be20e4d4531b9716aa376bedbb1f

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -333,6 +333,9 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
 
+        if (mHandler == null)
+            mHandler = new Handler();
+
         if (!mAttached) {
             mAttached = true;
             IntentFilter filter = new IntentFilter();
@@ -353,8 +356,6 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
             }
             mCurrentUserTracker.startTracking();
             mCurrentUserId = mCurrentUserTracker.getCurrentUserId();
-
-            mHandler = new Handler();
         }
 
         // NOTE: It's safe to do these after registering the receiver since the receiver always runs
@@ -397,6 +398,10 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
+
+            if (mHandler == null)
+                mHandler = new Handler();
+
             if (mHandler == null) {
                 Log.e(TAG,
                         "Received intent, but handler is null - still attached to window? Window "
